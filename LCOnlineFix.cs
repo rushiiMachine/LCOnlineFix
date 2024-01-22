b9dbb9c363fc391c166ebec6d982aefe9b18220f
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2024-01-21 22:01:29 -0800

    chore: init project

---
+++ LCOnlineFix.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace LCOnlineFix;
+
+[BepInPlugin("lconlinefix", "LCOnlineFix", "1.0.0")]
+[BepInProcess("Lethal Company.exe")]
+public class LCOnlineFix : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Harmony.CreateAndPatchAll(typeof(LCOnlineFix).Assembly)
+    }
+}
