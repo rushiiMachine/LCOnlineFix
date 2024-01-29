cd4aa4f93bda743b0cbb4fb2b72d60299694d07b
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2024-01-28 20:19:55 -0800

    feat: remove BepInEx executable filter
    This could be used on any game that uses that Facepunch networking library

--- LCOnlineFix.cs
+++ LCOnlineFix.cs
@@ -7,7 +7,6 @@ using HarmonyLib;
 namespace LCOnlineFix;
 
 [BepInPlugin("lconlinefix", "LCOnlineFix", "1.0.0")]
-[BepInProcess("Lethal Company.exe")]
 public class LCOnlineFix : BaseUnityPlugin
 {
     private void Awake() => Harmony.CreateAndPatchAll(typeof(LCOnlineFix).Assembly, "lconlinefix");
